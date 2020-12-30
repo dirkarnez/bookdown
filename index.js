43f9a27b91d85b7344df87276e080af64f58a91c
Dirk Arnez <smalldirkalex@gmail.com>
2020-12-30 17:34:16 +0800

    - add index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Bookmark as markdown
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.addEventListener('yt-page-data-updated', function() {
+        const button = document.createElement("button");
+        button.innerText = "hahah";
+        button.addEventListener("click", function() {
+            navigator.clipboard
+            .writeText(`- [${document.title}](${window.location.href})`)
+            .then(() => {
+                 alert("Done!");
+             })
+            .catch(err => {
+                 alert(err);
+             });
+        });
+        document.querySelector("#container > h1").appendChild(button);
+    });
+})();
